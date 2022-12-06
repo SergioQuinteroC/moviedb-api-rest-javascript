a63727c33a99b71f1890bbf4950ae7dd171565e5
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2022-12-05 19:43:22 -0500

    add movies detail

--- src/main.js
+++ src/main.js
@@ -18,6 +18,9 @@ function createMovies(movies, container) {
     movies.forEach((movie) => {
         const movieContainer = document.createElement("div");
         movieContainer.classList.add("movie-container");
+        movieContainer.addEventListener("click", () => {
+            location.hash = "#movie=" + movie.id;
+        });
 
         const movieImg = document.createElement("img");
         movieImg.classList.add("movie-img");
@@ -99,40 +102,31 @@ async function getTrendingMovies() {
     createMovies(movies, genericSection);
 }
 
-/* getAndAppendMovies (path, parentContainer, optionalConfig = {}){
-    const {data} = await api(path,optionalConfig)
+async function getMovieById(id) {
+    const { data: movie } = await api(`/movie/${id}`);
+
+    const movieImgUrl = `https://image.tmdb.org/t/p/w500` + movie.poster_path;
+    headerSection.style.background = `
+    linear-gradient(
+        180deg,
+        rgba(0, 0, 0, 0.35) 19.27%,
+        rgba(0, 0, 0, 0) 29.17%
+    ),
+    url(${movieImgUrl})`;
+
+    movieDetailTitle.textContent = movie.title;
+    movieDetailDescription.textContent = movie.overview;
+    movieDetailScore.textContent = movie.vote_average;
+
+    createCategories(movie.genres, movieDetailCategoriesList);
+
+    getRelatedMoviesById(id);
 }
-    getAndAppendMovies('/trending/movie/day', trendingMoviesPreviewList)
-    getAndAppendMovies('/discover/movie', genericSection , {
-        params: {
-            with_genres:id
-        }
-    })
 
-    async function getMoviesByCategory(id) {
-    const { data } = await api(`/discover/movie`, {
-        params: {
-            with_genres: id
-        }
-    });
-    const movies = data.results;
+async function getRelatedMoviesById(id) {
+    const { data } = await api(`/movie/${id}/similar`);
 
-    genericSection.innerHTML = "";
+    const relatedMovies = data.results;
 
-    movies.forEach((movie) => {
-        const movieContainer = document.createElement("div");
-        movieContainer.classList.add("movie-container");
-
-        const movieImg = document.createElement("img");
-        movieImg.classList.add("movie-img");
-        movieImg.setAttribute("alt", movie.tittle);
-        movieImg.setAttribute(
-            "src",
-            `https://image.tmdb.org/t/p/w300` + movie.poster_path
-        );
-
-        movieContainer.appendChild(movieImg);
-        genericSection.appendChild(movieContainer);
-    });
+    createMovies(relatedMovies, relatedMoviesContainer);
 }
- */
--- src/navigation.js
+++ src/navigation.js
@@ -108,6 +108,10 @@ function movieDetailsPage() {
     categoriesPreviewSection.classList.add("inactive");
     genericSection.classList.add("inactive");
     movieDetailSection.classList.remove("inactive");
+
+    const [, movieId] = location.hash.split("=");
+
+    getMovieById(movieId);
 }
 function searchPage() {
     console.log("Search!!!");
